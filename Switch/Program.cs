e8c65850d08178d0e626c5bf048e55745a385250
İsmail Can Yıldırım <ismailcan5106@gmail.com>
2022-10-15 16:54:12 +0300

    Add project files.

---
+++ Switch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            switch (number)
+            {
+
+                case 5:
+                    Console.WriteLine(number);
+                    Console.ReadLine();
+                    break;
+                case 10:
+                    Console.WriteLine(number + 1);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine(number + 999);
+                    Console.ReadLine();
+                    break;
+
+
+            }
+        }
+    }
+}
